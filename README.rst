9eaa97e1297fa0c502ec7cfc9e7a26b715fada88
Lucas <lucas@lucaswilkins.com>
2022-05-20 16:52:57 +0100

    Added changable sizes

--- README.rst
+++ README.rst
@@ -11,5 +11,8 @@ Usage
   # pipe the output from calling the script with some names to a file
   python3 create_latex.py Alice Bob Charlie > badges.tex
 
+  # ... or you can specify a size in mm
+  python3 create_latex.py 58 Alice Bob Charlie > badges.tex
+
   # build the file with pdflatex
   pdflatex badges
--- create_latex.py
+++ create_latex.py
@@ -2,6 +2,13 @@ import sys
 
 names = sys.argv[1:]
 
+# Hacky way of providing a size input
+try:
+    size = int(names[0])
+    names = names[1:]
+except:
+    size = 38
+
 test_names = [
     "Alice",
     "Bob",
@@ -26,7 +33,7 @@ footer = r"""
 \end{document}
 """
 
-def badge_entry(name: str, size: int):
+def badge_entry(name: str):
     pic = r"\includegraphics[width=%imm, height=%imm]{logo.pdf}"%(size, size)
     label = r"\vspace{%gmm}\textbf{\textsf{\Huge %s}}"%(-size/2-3.5, name)
     space = r"\vspace{%imm}"%size
@@ -34,6 +41,6 @@ def badge_entry(name: str, size: int):
 
 print(header)
 for name in names:
-    print(badge_entry(name, 38))
+    print(badge_entry(name))
 print(footer)
 
